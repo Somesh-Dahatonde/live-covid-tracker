4550d217077dc9730f53462d37d55f999e7bb9dd
Deven Rathod <devenrathod2001@gmail.com>
2021-08-11 23:57:05 +0530

    made some changes in axios

--- src/Components/Covidapi.jsx
+++ src/Components/Covidapi.jsx
@@ -1,31 +1,64 @@
-import React from 'react';
-
-
+import { React, useEffect, useState } from "react";
+import axios from "axios";
 
 const Covidapi = () => {
-    // let [newData , setnewData]= useState([]);
+    const [statewise, setStatewise] = useState([]);
 
-    const getData = async () => {
-        try{
-        const data = await fetch("https://api.covid19india.org/data.json");
-        let actualData = await data.json();
-    //    setnewData(actualData);
-    console.log(actualData.statewise)
-    return(
+    axios
+        .get(
+            "https://api.covid19india.org/data.json",
+            {},
+            {
+                headers: { "Content-Type": "application/json; charset=utf-8" },
+            }
+        )
+        .then((response) => {
+            // console.log(response);
+            setStatewise(response.data.statewise);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+    return (
         <>
-        <div>Hello</div>
-        <div>{actualData.statewise[0]}</div>
-        </> 
-       );
-       
-    }catch(error){
-        console.log(error)
-    }
+            <div>
+                <h1>API CALLING </h1>
+                {statewise.map((item) => {
+                    
+                    return (
+                        <div key={item.id}>
+                            <p>Active Cases {item.active}</p>
+                        </div>
+                    );
+                })}
+            </div>
+        </>
+    );
 
-}
-    getData();
-    return(
-        <h1> hey </h1>
-    )
-}
+    //     const getData = async () => {
+    //         try{
+    //         const data = await fetch("https://api.covid19india.org/data.json");
+    //         let actualData = await data.json();
+    //         stateWise = await data.json();
+    //         console.log(actualData.statewise[0].active)
+    //     //    setnewData(actualData);
+    //     // console.log(actualData.statewise)
+    //     return(
+    //         <>
+    //         <div>Hello</div>
+    //         <div>{actualData.statewise[0]}</div>
+    //         </>
+    //        );
+
+    //     }catch(error){
+    //         console.log(error)
+    //     }
+
+    // }
+    // getData();
+    // return(
+    //     <h1> hey </h1>
+    // )
+};
 export default Covidapi;
